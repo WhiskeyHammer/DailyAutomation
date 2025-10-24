7253c11b30e4d8127aaf18d82b5d0e02117ef167
Jeremy Wiggins <jewiggins@greenshades.com>
2025-10-24 13:41:12 -0400

    config update

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import nodriver as uc
+import os
+import asyncio
 
 async def main():
-    sandbox = False
-    browser = await uc.start(no_sandbox=True)
+    print("Starting browser...")
+    print(f"DISPLAY: {os.environ.get('DISPLAY')}")
+    print(f"CHROME_PATH: {os.environ.get('CHROME_PATH')}")
+    
+    # Method 1: Use Config object (RECOMMENDED)
+    config = uc.Config()
+    config.browser_executable_path = os.environ.get('CHROME_PATH')
+    config.browser_args = [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--no-first-run',
+        '--no-default-browser-check',
+        '--window-size=1920,1080'
+    ]
+
+    browser = await uc.start(config)
     page = await browser.get('https://news.google.com/home?hl=en-US&gl=US&ceid=US:en')
 
     await page.save_screenshot()
